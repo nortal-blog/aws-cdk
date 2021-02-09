9cdb04d3a3bb074b05be243cbb78a426f8c82592
O-P Tuuttila <olli-petteri.tuuttila@nortal.com>
2021-02-09 17:16:26 +0200

    Initial commit

---
+++ src/Src/SrcStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace Src
+{
+    public class SrcStack : Stack
+    {
+        internal SrcStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
